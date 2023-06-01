708be4c63eb69a0b78c8ed58c699f1fe79752e93
Daniel Yankov <yankov27@gmail.com>
2023-06-01 06:45:33 +0300

    Beggining of the project

---
+++ src/main/java/com/example/server/model/Server.java
@@ -0,0 +1,34 @@
+package com.example.server.model;
+
+import com.example.server.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty")
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+
+}
