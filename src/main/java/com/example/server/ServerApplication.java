8ac16bbb95c4c6632fedfa56696947f9a54ca7d5
Daniel Yankov <yankov27@gmail.com>
2023-06-05 04:33:07 +0300

    Project progress

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -1,13 +1,25 @@
 package com.example.server;
 
+import com.example.server.enumeration.Status;
+import com.example.server.model.Server;
+import com.example.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner run(ServerRepo serverRepo) {
+        return args -> {
+            serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "15 GB", "Personal PC",
+                    "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+        };
+    }
 }
